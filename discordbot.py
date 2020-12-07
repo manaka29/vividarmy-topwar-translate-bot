a7f9ec31d18391f968c17a3c0928c6e275cb3edf
manaka29 <kensuke.sylph@gmail.com>
2020-12-07 17:34:06 +0900

    first commit

--- discordbot.py
+++ discordbot.py
@@ -1,10 +1,16 @@
 from discord.ext import commands
 import os
 import traceback
+import json
+import unicodedata
 
-bot = commands.Bot(command_prefix='/')
+bot = commands.Bot(command_prefix='$')
 token = os.environ['DISCORD_BOT_TOKEN']
 
+vividarmy_file = open('data/vividarmy.json','r')
+vividarmy = json.load(vividarmy_file)
+topwar_file = open('data/topwar.json', 'r')
+topwar = open(topwar_file)
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -16,6 +22,18 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+
+
+@bot.command()
+async def vividarmy(ctx, *args):
+    response = ''
+    for arg in args:
+        normalized = unicodedata.normalize('NKFC', arg)
+        if normalized not in vividarmy:
+            response += '{} : NOT DEFINED\n'.format(normalized)
+        else
+            response += '{} : {}\n'.format(normalized, vividarmy[normalized])
+    await ctx.send(response.rstrip('\n'))
 
 
 bot.run(token)
