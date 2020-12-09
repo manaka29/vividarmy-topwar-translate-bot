c3d133109226c71c290082b15b7381a0fc6ce560
manaka29 <kensuke.sylph@gmail.com>
2020-12-09 11:48:06 +0900

    Fit to flake8

--- discordbot.py
+++ discordbot.py
@@ -7,15 +7,19 @@ import unicodedata
 bot = commands.Bot(command_prefix='$')
 token = os.environ['DISCORD_BOT_TOKEN']
 
-vividarmy_file = open('./data/vividarmy.json','r', encoding="utf-8")
+vividarmy_file = open('./data/vividarmy.json', 'r', encoding="utf-8")
 vividarmy_dict = json.load(vividarmy_file)
-topwar_file    = open('./data/topwar.json', 'r', encoding="utf-8")
-topwar_dict    = json.load(topwar_file)
+topwar_file = open('./data/topwar.json', 'r', encoding="utf-8")
+topwar_dict = json.load(topwar_file)
+
 
 @bot.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    error_msg = ''.join(traceback
+                        .TracebackException
+                        .from_exception(orig_error)
+                        .format())
     await ctx.send(error_msg)
 
 
@@ -32,7 +36,8 @@ async def _vividarmy(ctx, *args):
         if normalized not in vividarmy_dict:
             response += '{} : NOT DEFINED\n'.format(normalized)
         else:
-            response += '{} : {}\n'.format(normalized, vividarmy_dict[normalized])
+            response += '{} : {}\n'.format(
+                normalized, vividarmy_dict[normalized])
     await ctx.send(response.rstrip('\n'))
 
 
@@ -40,12 +45,13 @@ async def _vividarmy(ctx, *args):
 async def _topwar(ctx, *args):
     response = ''
     for arg in args:
-        normalized = unicodedata.normalize('NFKC', arg).lower().replace(' ', '-').replace('\'','').replace('.','')
+        normalized_1 = unicodedata.normalize('NFKC', arg).lower()
+        normalized_2 = normalized_1.replace(' ', '-')
+        normalized = normalized_2.replace('\'', '').replace('.', '')
         if normalized not in topwar_dict:
             response += '{} : NOT DEFINED\n'.format(arg)
         else:
-            response += '{} : {}\n'.format(arg
-            , topwar_dict[normalized])
+            response += '{} : {}\n'.format(arg, topwar_dict[normalized])
     await ctx.send(response.rstrip('\n'))
 
 
